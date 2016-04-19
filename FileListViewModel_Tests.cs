4d8d4f517e20ef34d58f898621c960cad4364011
Justin <uvatmvf@gmail.com>
2016-04-18 20:01:46 -0600

    Added unit tests for Save functions on view model class

--- FileListViewModel_Tests.cs
+++ FileListViewModel_Tests.cs
@@ -4,11 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfXmlDemo.ViewModels;
 
 namespace WpfXmlDemo.UnitTest
 {
     [TestFixture]
-    public class TestClass
+    public class FileListViewModel_Tests
     {
+        [Test]
+        public void VMCanSave_Test()
+        {
+            FileListViewModel vm = new FileListViewModel("", "");
+            Assert.DoesNotThrow(() => { bool test = vm.CanSave; });
+        }
+
+        [Test]
+        public void VMSave_Test()
+        {
+            FileListViewModel vm = new FileListViewModel("", "");
+            Assert.Throws<NotImplementedException>(() => { vm.Save(); });
+        }
     }
 }
